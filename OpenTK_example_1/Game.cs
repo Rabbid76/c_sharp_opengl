0aeeb6bc72c5e02ec4e81710f5da1193015c1778
Rabbid76 <Gernot.Steinegger@gmail.com>
2019-11-01 22:21:47 +0100

    initial

---
+++ OpenTK_example_1/Game.cs
@@ -0,0 +1,180 @@
+﻿
+//! Creating a Window
+//! [https://opentk.net/learn/chapter1/1-creating-a-window.html]
+//!
+//! .NET Framework 2.0
+//! TOOLS / NuGet / Package Manager Console
+//! Install-Package OpenTK
+
+//! Hello Triangle
+//! [https://opentk.net/learn/chapter1/2-hello-triangle.html]
+
+using OpenTK;
+using OpenTK.Input;            // KeyboardState, Keyboard, Key
+using OpenTK.Graphics;         // GameWindow, GraphicsMode, Context
+using OpenTK.Graphics.OpenGL4; // GL
+
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace OpenTK_example_1
+{
+  public class Game
+    : GameWindow
+    , IDisposable
+  {
+    bool _disposedValue = false;
+
+    Shader _test_prog;
+
+    int _test_vao;
+    int _test_vbo;
+    int _test_ibo;
+
+
+    public Game(int width, int height, string title)
+      : base(width, height, GraphicsMode.Default, title,
+             GameWindowFlags.Default,
+             DisplayDevice.Default,
+             4,
+             6,
+             GraphicsContextFlags.Default | GraphicsContextFlags.Debug)
+    { }
+
+    public void Dispose()
+    {
+      this.Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (disposing && !this._disposedValue)
+      {
+        _test_prog.Dispose();
+
+        int[] buffers = { this._test_vbo, this._test_ibo };
+        GL.DeleteBuffers(2, buffers);
+        GL.DeleteVertexArray(_test_vao);
+
+        this._disposedValue = true;
+      }
+    }
+
+    // Callback for OpenGL debug message
+    public static void DebugProc(DebugSource source, DebugType type, int id, DebugSeverity severity, int length, IntPtr message, IntPtr userParam)
+    {
+      string message_str = Marshal.PtrToStringAnsi(message);
+      Console.WriteLine(message_str);
+    }
+
+    //! On load window (once)
+    protected override void OnLoad(EventArgs e)
+    {
+      // debug callback
+
+      GL.DebugMessageCallback(DebugProc, IntPtr.Zero);
+
+      // filter: all debug messages on
+      GL.DebugMessageControl(DebugSourceControl.DontCare, DebugTypeControl.DontCare, DebugSeverityControl.DontCare, 0, new int[0], true);
+
+      // filter: only error messages
+      //GL.DebugMessageControl(DebugSourceControl.DontCare, DebugTypeControl.DontCare, DebugSeverityControl.DontCare, 0, new int[0], false);
+      //GL.DebugMessageControl(DebugSourceControl.DebugSourceApi, DebugTypeControl.DebugTypeError, DebugSeverityControl.DontCare, 0, new int[0], false);
+
+      GL.Enable(EnableCap.DebugOutput);
+      GL.Enable(EnableCap.DebugOutputSynchronous);
+      GL.DebugMessageInsert(DebugSourceExternal.DebugSourceApplication, DebugType.DebugTypeMarker, 0, DebugSeverity.DebugSeverityNotification, -1, "Debug output enabled");
+
+      // create Vertex Array Object, Array Buffer Object and Element Array Buffer Object
+
+      float[] vquad = 
+      {
+      // x      y     z      r     g     b     a
+        -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f, 
+         0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 0.0f, 1.0f,
+         0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 1.0f
+      };
+
+      uint [] iquad = { 0, 1, 2, 0, 2, 3 };
+
+      this._test_vao = GL.GenVertexArray();
+      this._test_vbo = GL.GenBuffer();
+      this._test_ibo = GL.GenBuffer();
+
+      GL.BindBuffer(BufferTarget.ArrayBuffer, this._test_vbo);
+      GL.BufferData(BufferTarget.ArrayBuffer, vquad.Length * sizeof(float), vquad, BufferUsageHint.StaticDraw);
+
+      GL.BindVertexArray(this._test_vao);
+
+      GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 7 * sizeof(float), 0);
+      GL.EnableVertexAttribArray(0);
+      GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 7 * sizeof(float), 3 * sizeof(float));
+      GL.EnableVertexAttribArray(1);
+
+      GL.BindBuffer(BufferTarget.ElementArrayBuffer, this._test_ibo);
+      GL.BufferData(BufferTarget.ElementArrayBuffer, iquad.Length * sizeof(uint), iquad, BufferUsageHint.StaticDraw);
+
+      // Create shader program
+
+      string vert_shader = @"#version 460 core
+      layout (location = 0) in vec4 a_pos;
+      layout (location = 1) in vec4 a_color;
+      
+      out vec4 v_color;
+
+      void main()
+      {
+          v_color     = a_color;
+          gl_Position = a_pos; 
+      }";
+
+      string frag_shader = @"#version 460 core
+      out vec4 frag_color;
+      in  vec4 v_color;
+      
+      void main()
+      {
+          frag_color = v_color; 
+      }";
+
+      this._test_prog = new Shader(vert_shader, frag_shader);
+      this._test_prog.Generate();
+
+      this._test_prog.Use();
+
+      // states
+
+      GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+      base.OnLoad(e);
+    }
+
+    //! On resize
+    protected override void OnResize(EventArgs e)
+    {
+      GL.Viewport(0, 0, this.Width,this.Height);
+      base.OnResize(e);
+    }
+
+    //! On update window
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+      KeyboardState input = Keyboard.GetState();
+      if (input.IsKeyDown(Key.Escape))
+      {
+        Exit();
+      }
+
+
+      GL.Clear(ClearBufferMask.ColorBufferBit);
+
+      GL.DrawElements(BeginMode.Triangles, 6, DrawElementsType.UnsignedInt, 0);
+
+      Context.SwapBuffers();
+      base.OnUpdateFrame(e);
+    }
+  }
+}
---
+++ OpenTK_example_1/Shader.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL4; // GL, ShaderType
+
+
+// TODO: map shader type sources / spire-v
+
+namespace OpenTK_example_1
+{
+  public class Shader
+    : IDisposable
+  {
+    public int program
+    {
+      get { return this._program; }
+    }
+    private int _program;
+    private bool _disposedValue = false;
+
+    private string _vert_source; //!< vertex shader source (TODO abstract, map)
+    private string _frag_source; //!< vertex shader source (TODO abstract, map)
+
+    public Shader(string VertexShaderSource, string FragmentShaderSource)
+    {
+      this._vert_source = VertexShaderSource;
+      this._frag_source = FragmentShaderSource;
+    }
+
+    ~Shader()
+    {
+      GL.DeleteProgram(this._program);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (disposing && !_disposedValue)
+      {
+        GL.DeleteProgram(this._program);
+        _disposedValue = true;
+      }
+    }
+
+    public void Dispose()
+    {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    // install program
+    public void Use()
+    {
+      GL.UseProgram(this._program);
+    }
+
+    // generate a shader program
+    public bool Generate()
+    {
+      int vert_shader = this.GenerateShader(ShaderType.VertexShader, _vert_source);
+      int frag_shader = this.GenerateShader(ShaderType.FragmentShader, _frag_source);
+
+      this.CompileShader(vert_shader);
+      this.CompileShader(frag_shader);
+
+      this._program = GL.CreateProgram();
+
+      GL.AttachShader(this._program, vert_shader);
+      GL.AttachShader(this._program, frag_shader);
+
+      GL.LinkProgram(this._program);
+      string infoLogProg = GL.GetProgramInfoLog(this._program);
+      if (infoLogProg != System.String.Empty)
+      {
+        System.Console.WriteLine(infoLogProg);
+        return false; // TODO exception
+      }
+
+      GL.DetachShader(this._program, vert_shader);
+      GL.DetachShader(this._program, frag_shader);
+      GL.DeleteShader(vert_shader);
+      GL.DeleteShader(frag_shader);
+
+      return true;
+    }
+
+    //! generate a shader object with a specific type
+    private int GenerateShader(ShaderType type, string source)
+    {
+      int shader = GL.CreateShader(type);
+      GL.ShaderSource(shader, source);
+      return shader;
+    }
+
+    //! compile a shader object 
+    private bool CompileShader(int shader)
+    {
+      GL.CompileShader(shader);
+
+      string infoLogVert = GL.GetShaderInfoLog(shader);
+      if (infoLogVert != System.String.Empty)
+      {
+        System.Console.WriteLine(infoLogVert);
+        return false; // TODO exception
+      }
+      return true;
+    }
+  }
+}
